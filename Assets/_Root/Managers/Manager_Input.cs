df5f84ef328dabddefcd95d40a64c5f943e470b8
AdalZaleta <32682010+AdalZaleta@users.noreply.github.com>
2018-07-16 16:55:22 -0500

    Controller Character

---
+++ Assets/_Root/Managers/Manager_Input.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+namespace TAAI
+{
+	public class Manager_Input : MonoBehaviour {
+
+		void Awake()
+		{
+			Manager_Static.inputManager = this;
+		}
+
+		void Update()
+		{
+			//CODIGO DE LOS INPUTS DEPENDIENDO DEL ESTADO DEL JUEGO
+			if (Manager_Static.appManager.currentState == AppState.pause_menu) {
+				if (Input.GetKeyUp (KeyCode.JoystickButton7)) {
+					Manager_Static.appManager.setPlay ();
+					Manager_Static.uiManager.isInPause (false);
+				}
+			}
+			else if (Manager_Static.appManager.currentState == AppState.main_menu) {
+			}
+			else if (Manager_Static.appManager.currentState == AppState.gameplay) {
+				if (Input.GetAxisRaw ("Horizontal") != 0.0f || Input.GetAxisRaw ("Vertical") != 0.0f) {
+                    Manager_Static.controllerManager.MoveCharacter(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+				}
+                if (Input.GetButtonDown("Fire2")) {
+                    Debug.Log("Dandole Fire2");
+                }
+				if (Input.GetKeyUp (KeyCode.JoystickButton7)) {
+					Manager_Static.appManager.SetPause ();
+					Manager_Static.uiManager.isInPause (true);
+				}
+			}
+			else if (Manager_Static.appManager.currentState == AppState.end_game) {
+			}
+		}
+
+		public delegate void InputTemplate (int _id);
+
+		public InputTemplate AchievementHandler;
+		public InputTemplate DecalChangeHandler;
+		public InputTemplate DecalHandler;
+	}
+}
---
+++ Assets/_Root/Managers/Manager_Static.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace TAAI
+{
+	public enum AppState
+	{
+		main_menu,
+		gameplay,
+		pause_menu,
+		end_game,
+		credits
+	}
+
+	public enum ControllerSide
+	{
+		player1 = 0,
+		player2 = 1
+	}
+
+	public static class Manager_Static 
+	{
+		public static Manager_Input inputManager;
+		public static Manager_App appManager;
+		public static Manager_Controller controllerManager;
+		public static Manager_Scene sceneManager;
+		public static Manager_Audio audioManager;
+		public static Manager_UI uiManager;
+	}
+}
